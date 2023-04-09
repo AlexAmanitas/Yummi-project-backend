1a910fe85728759d21c69abd6820be4b177ad90f
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-04-09 14:38:11 +0300

    shopping-list

--- controllers/shopingList.js
+++ controllers/shopingList.js
@@ -20,43 +20,47 @@ const getShopingList = async (req, res) => {
 
 const addShopingList = async (req, res) => {
   const { _id } = req.user;
-  const { id, quantity, unit } = req.body;
-  const ingredient = await Ingredient.findOne(ObjectId(id));
-  const user = await User.findOne(_id);
+  // const { id, ttl, thb, recipe, measure } = req.body;
+  // const ingredient = await Ingredient.findOne(ObjectId(id));
+  // console.log(id, ttl, thb, recipe, measure);
+  // const user = await User.findOne(_id);
 
-  const availability = await User.find({ shopingList: { $elemMatch: { id } } });
-  if (availability.length === 0) {
-    await User.updateOne(
-      { _id },
-      {
-        $push: {
-          shopingList: {
-            id,
-            name: ingredient.ttl,
-            quantity,
-            unit,
-            image: ingredient.thb,
-          },
-        },
-      }
-    );
-  } else {
-    const quanty = availability[0].shopingList.find(
-      item => item.id === id
-    ).quantity;
-    const newQuantity = quantity + quanty;
-    console.log(newQuantity);
-    await User.updateOne(
-      { _id, 'shopingList.id': id },
-      { $set: { 'shopingList.$.quantity': quantity + quanty } },
-      { new: true }
-    );
-  }
+  // user.shopingList = { ...req.body };
+  await User.updateOne({ _id }, { $push: { shopingList: { ...req.body } } });
+
+  // const availability = await User.find({ shopingList: { $elemMatch: { id } } });
+  // if (availability.length === 0) {
+  //   await User.updateOne(
+  //     { _id },
+  //     {
+  //       $push: {
+  //         shopingList: {
+  //           id,
+  //           name: ingredient.ttl,
+  //           quantity,
+  //           unit,
+  //           image: ingredient.thb,
+  //         },
+  //       },
+  //     }
+  //   );
+  // } else {
+  //   const quanty = availability[0].shopingList.find(
+  //     item => item.id === id
+  //   ).quantity;
+  //   const newQuantity = quantity + quanty;
+  //   console.log(newQuantity);
+  //   await User.updateOne(
+  //     { _id, 'shopingList.id': id },
+  //     { $set: { 'shopingList.$.quantity': quantity + quanty } },
+  //     { new: true }
+  //   );
+  // }
 
   res.status(201).json({
     status: 'success',
     code: 201,
-    data: { user: user.shopingList },
+    data: { ...req.body },
   });
 };
 
